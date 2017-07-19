8e86f3e1cba8ebe5367f11c0a8aff3df0c74063c
Daiji Fukagawa <dfukagaw@mail.doshisha.ac.jp>
2017-07-19 23:51:46 +0900

    initial commit

---
+++ MailMimeEx.php
@@ -0,0 +1,156 @@
+<?php
+declare(strict_types=1);
+
+namespace dfkgw;
+
+use \Mail_mime;
+
+class MailMimeEx
+{
+    protected $message;
+
+    public function __construct(Mail_mime $message)
+    {
+        $this->message = $message;
+    }
+
+    /* ======== Set/get MIME headers ======== */
+
+    /**
+     * Get encoded headers
+     */
+    public function getHeaders()
+    {
+        return $this->message->headers();
+    }
+
+    /**
+     * Get raw headers (in a bad-mannered way)
+     */
+    public function getRawHeaders()
+    {
+        $message_array = (array)$this->message;
+        return $message_array["\0*\0headers"];
+    }
+
+    /**
+     * Set headers
+     */
+    public function setHeaders($headers)
+    {
+        $encodedHeaders = $this->message->headers($headers, true);
+        return $encodedHeaders;
+    }
+
+    /**
+     * Get text body
+     */
+    public function getTextBody()
+    {
+        return $this->message->getTXTBody();
+    }
+
+    /**
+     * Set text body
+     */
+    public function setTextBody($text)
+    {
+        $this->message->setTXTBody($text);
+    }
+
+    /* ======== Set/get parameters ======== */
+
+    public function setParam($name, $new_value)
+    {
+        $this->message->setParam($name, $new_value);
+    }
+
+    /* ======== Set/get charset parameters ======== */
+
+    public function getHeaderCharset()
+    {
+        return $this->_getCharset('head_charset');
+    }
+
+    public function getTextCharset()
+    {
+        return $this->_getCharset('text_charset');
+    }
+
+    private function _getCharset($name)
+    {
+        $charset = $this->message->getParam($name);
+        if (empty($charset)) {
+            $charset = 'US-ASCII';
+        } else {
+            $pos = strpos($charset, ';');
+            if ($pos !== false) {
+                $charset = substr($charset, 0, $pos);
+            }
+        }
+        return $charset;
+    }
+
+    /* ======== Convert character encodings ======== */
+
+    public function updateHeaderCharset(string $new_charset, string $current_charset=null)
+    {
+        if (empty($current_charset)) {
+            $current_charset = $this->getHeaderCharset();
+        }
+        if ($new_charset != $current_charset) {
+            $this->setParam('head_charset', $new_charset);
+            $this->_convertHeaders($new_charset, $current_charset);
+        }
+    }
+
+    public function updateTextCharset(string $new_charset, string $current_charset=null)
+    {
+        if (empty($current_charset)) {
+            $current_charset = $this->getTextCharset();
+        }
+        if ($new_charset != $current_charset) {
+            $this->setParam('text_charset', $new_charset);
+            $this->_convertTextBody($new_charset, $current_charset);
+        }
+    }
+
+    private function _convertHeaders(string $new_charset, string $current_charset=null)
+    {
+        $headers = $this->getRawHeaders();
+        $changed = false;
+        foreach ($headers as $key => $val) {
+            $new_val = self::_convert($val, $new_charset, $current_charset);
+            if ($val != $new_val) {
+                $headers[$key] = $new_val;
+                $changed = true;
+            }
+        }
+        if ($changed) {
+            $this->setHeaders($headers);
+        }
+    }
+
+    private function _convertTextBody(string $new_charset, string $current_charset=null)
+    {
+        $text = $this->getTextBody();
+        $text = self::_convert($text, $new_charset, $current_charset);
+        $this->setTextBody($text);
+    }
+
+    private static function _convert($value, $to_encoding, $from_encoding)
+    {
+        if (is_array($value)) {
+            $value_new = [];
+            foreach ($value as $val) {
+                $value_new[] = self::_convert($val, $to_encoding, $from_encoding);
+            }
+        } else {
+            if (empty($from_encoding)) {
+                $from_encoding = mb_internal_encoding();
+            }
+            $value_new = mb_convert_encoding($value, $to_encoding, $from_encoding);
+        }
+        return $value_new;
+    }
+}
---
+++ tests/MailMimeExTest.php
@@ -0,0 +1,185 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use dfkgw\MailMimeEx;
+
+final class MailMimeExTest extends TestCase
+{
+    private static function initMessage(
+        array $headers = array(),
+        string $text = ''
+    ): Mail_mime
+    {
+        // Set up a message
+        $message = new Mail_mime("\r\n");
+
+        // Set message body
+        $message->setTXTBody($text);
+
+        // Set encodings
+        $message->setParam('text_encoding', '8bit');
+        $message->setParam('html_encoding', 'quoted-printable');
+        $message->setParam('head_encoding', 'quoted-printable');
+        $message->setParam('head_charset', 'UTF-8');
+        $message->setParam('html_charset', 'UTF-8');
+        $message->setParam('text_charset', 'UTF-8');
+
+        // Set headers
+        $message->headers($headers);
+
+        return $message;
+    }
+
+    public function testCanLoad(): void
+    {
+        $message = self::initMessage(
+            array(), ''
+        );
+        $message = new MailMimeEx($message);
+        $this->assertInstanceOf(MailMimeEx::class, $message);
+    }
+
+    public function testCanGetSubject(): void
+    {
+        $message = self::initMessage(array('Subject' => 'テストメールです'), 'こんにちは');
+        $message = new MailMimeEx($message);
+
+        // raw subject
+        $headers = $message->getRawHeaders();
+        $this->assertArrayHasKey('Subject', $headers);
+        $subject = $headers['Subject'];
+        $expected = 'テストメールです';
+        $this->assertEquals($expected, $subject);
+    }
+
+    public function testCanEncodeSubject_utf8_q(): void
+    {
+        $message = self::initMessage(array('Subject' => 'テストメールです'), 'こんにちは');
+        $message = new MailMimeEx($message);
+
+        // encoded subject
+        $headers = $message->getHeaders();
+        $this->assertArrayHasKey('Subject', $headers);
+        $subject = $headers['Subject'];
+        $expected = join("\r\n ",
+            array(
+                '=?UTF-8?Q?=E3=83=86=E3=82=B9=E3=83=88=E3=83=A1=E3=83=BC=E3=83=AB?=',
+                '=?UTF-8?Q?=E3=81=A7=E3=81=99?='
+            ));
+        $this->assertEquals($expected, $subject);
+    }
+
+    public function testCanEncodeSubject_jis_b(): void
+    {
+        $message = self::initMessage(array('Subject' => 'テストメールです'), 'こんにちは');
+        $message = new MailMimeEx($message);
+
+        // Change charset
+        $message->getHeaderCharset();
+        $message->setParam('head_encoding', 'base64');
+        $message->updateHeaderCharset('ISO-2022-JP');
+        $subject = $message->getRawHeaders()['Subject'];
+        $expected = "\x1B\$B".'%F%9%H%a!<%k$G$9'."\x1B(B";
+        //$expected = mb_convert_encoding('テストメールです', 'ISO-2022-JP', 'UTF-8');
+        $this->assertEquals($expected, $subject);
+    }
+
+    // Sending ISO-2022-JP-MS message as if it is encoded by ISO-2022-JP
+    // (NOT RECOMMENDED for public use, but practically useful for communication
+    //  within some communities, because ISO-2022-JP-MS accepts JIS X 0213 characters
+    //  which contains more characters that JIS X 0208)
+    public function testCanEncodeSubject_cp932_b(): void
+    {
+        $message = self::initMessage(array('Subject' => 'テストメ〜ル①㈱'), 'こんにちは');
+        $message = new MailMimeEx($message);
+
+        // Change charset
+        $message->getHeaderCharset();
+        $message->setParam('head_encoding', 'base64');
+        $message->updateHeaderCharset('ISO-2022-JP-MS');
+        // This is necessary because "ISO-2022-JP-MS" is not valid
+        // for encoding name in MIME header
+        $message->setParam('head_charset', 'ISO-2022-JP');
+
+        $subject = $message->getRawHeaders()['Subject'];
+        $expected = "\x1B\$B".'%F%9%H%a!A%k-!-j'."\x1B(B";
+        //$expected = mb_convert_encoding('テストメ〜ル①㈱', 'ISO-2022-JP-MS', 'UTF-8');
+        $this->assertEquals($expected, $subject);
+    }
+
+    public function testCanGetTextBody(): void
+    {
+        $message = self::initMessage(array(), 'こんにちは');
+        $message = new MailMimeEx($message);
+
+        // raw body
+        $text = $message->getTextBody();
+        $expected = 'こんにちは';
+        $this->assertEquals($expected, $text);
+    }
+
+    public function testCanEncodeTextBody_jis(): void
+    {
+        $message = self::initMessage(array(), 'こんにちは');
+        $message = new MailMimeEx($message);
+
+        $message->setParam('text_encoding', '7bit');
+        $message->updateTextCharset('ISO-2022-JP');
+
+        // encoded body
+        $text = $message->getTextBody();
+        $expected = "\x1B\$B".'$3$s$K$A$O'."\x1B(B";
+        //$expected = mb_convert_encoding('こんにちは', 'ISO-2022-JP', 'UTF-8');
+        $this->assertEquals($expected, $text);
+    }
+
+    // Sending ISO-2022-JP-MS message as if it is encoded by ISO-2022-JP
+    // (NOT RECOMMENDED)
+    public function testCanEncodeTextBody_cp932(): void
+    {
+        $message = self::initMessage(array(), 'こんにちは①髙﨑');
+        $message = new MailMimeEx($message);
+
+        $message->setParam('text_encoding', '7bit');
+        $message->updateTextCharset('ISO-2022-JP-MS');
+        // This is necessary because "ISO-2022-JP-MS" is not valid
+        // for Content-Type charset
+        $message->setParam('text_charset', 'ISO-2022-JP');
+
+        // encoded body
+        $text = $message->getTextBody();
+        $expected = "\x1B\$B".'$3$s$K$A$O-!|byu'."\x1B(B";
+        //$expected = mb_convert_encoding('こんにちは①髙﨑', 'ISO-2022-JP-MS', 'UTF-8');
+        $this->assertEquals($expected, $text);
+    }
+
+    public function testCanComposeFlowedBody(): void
+    {
+        $message = self::initMessage(array(), 'Hello world!');
+        $message = new MailMimeEx($message);
+
+        $message->setTextBody('Hello \r\nworld!');
+        $message->setParam('text_encoding', '7bit');
+        $message->setParam('text_charset', 'US-ASCII; format=flowed');
+        $headers = $message->setHeaders(array());
+
+        // Check text body
+        $text = $message->getTextBody();
+        $expected = 'Hello \r\nworld!';
+        $this->assertEquals($expected, $text);
+
+        // Check headers
+        $headers = $message->getRawHeaders();
+        // content-type
+        $key = 'Content-Type';
+        $expected = 'text/plain; charset=US-ASCII; format=flowed';
+        $this->assertArrayHasKey($key, $headers);
+        $this->assertEquals($expected, $headers[$key]);
+        // content-transfer-encoding
+        $key = 'Content-Transfer-Encoding';
+        $expected = '7bit';
+        $this->assertArrayHasKey($key, $headers);
+        $this->assertEquals($expected, $headers[$key]);
+    }
+}
